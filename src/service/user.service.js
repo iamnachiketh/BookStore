11fc710b6cb2d1e970152d74d4761a1846ce87f0
iamnachiketh <nachikethkc@gmail.com>
2025-03-03 12:15:58 +0530

    [Nachiketha]Update: Single function added for DB operation instead of two

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -8,17 +8,18 @@ const { StatusCodes } = require("http-status-codes");
 const registerUser = async function (userData) {
     try {
 
-        const isUserExist = await User.findOne({ where: { email: userData.email } });
+        const [newUser, created] = await User.findOrCreate({
+            where: { email: userData.email },
+            defaults: userData
+        });
 
-        if (isUserExist) {
+        if (!created) {
             const error = new Error("User already exist");
             error.status = StatusCodes.CONFLICT;
             throw error;
         }
 
-        const user = await User.create(userData);
-
-        return { status: StatusCodes.CREATED, message: "User created successfully", data: user };
+        return { status: StatusCodes.CREATED, message: "User created successfully", data: newUser };
 
     } catch (error) {
         return { status: error.status || StatusCodes.INTERNAL_SERVER_ERROR, message: error.message, data: null };
@@ -68,7 +69,6 @@ const loginUser = async function (userData) {
         return { status: error.status || StatusCodes.INTERNAL_SERVER_ERROR, message: error.message, data: null, token: null };
     }
 }
-
 
 module.exports = {
     registerUser,
